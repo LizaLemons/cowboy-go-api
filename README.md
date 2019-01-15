55793d2a5a8b2ba0821a9ba996749a890b563426
Liza Ramo <liza.h.ramo@gmail.com>
2019-01-15 15:30:42 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+
+# Notes
+
+
+### To run: 
+
+```
+go build && ./cowboy-go-api
+```
+
+---
+
+### Postman
+
+```
+GET to localhost:8080/people
+```
+
+etc. 
+
+
+---
+
+
+### Resources
+
+Tutorial [here](https://www.codementor.io/codehakase/building-a-restful-api-with-golang-a6yivzqdo)
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+// mux routes our requests
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Cowboy struct {
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+var cowboys []Cowboy
+
+//
+func GetCowboys(w http.ResponseWriter, r *http.Request) {
+	log.Println(cowboys)
+	json.NewEncoder(w).Encode(cowboys)
+}
+
+func GetCowboy(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range cowboys {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Cowboy{})
+}
+
+func CreateCowboy(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var cowboy Cowboy
+	_ = json.NewDecoder(r.Body).Decode(&cowboy)
+	cowboy.ID = params["id"]
+	cowboys = append(cowboys, cowboy)
+	json.NewEncoder(w).Encode(cowboys)
+}
+
+func DeleteCowboy(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index, item := range cowboys {
+		if item.ID == params["id"] {
+			cowboys = append(cowboys[:index], cowboys[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(cowboys)
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	cowboys = append(cowboys, Cowboy{ID: "1", Firstname: "Billy", Lastname: "The Kid", Address: &Address{City: "New York City", State: "NY"}})
+	cowboys = append(cowboys, Cowboy{ID: "2", Firstname: "Will", Lastname: "Rogers", Address: &Address{City: "Oologah", State: "Oklahoma"}})
+	cowboys = append(cowboys, Cowboy{ID: "3", Firstname: "Jesse", Lastname: "James", Address: &Address{City: "Kearney", State: "Missouri"}})
+
+	router.HandleFunc("/cowboys", GetCowboys).Methods("GET")
+	router.HandleFunc("/cowboys/{id}", GetCowboy).Methods("GET")
+	router.HandleFunc("/cowboys/{id}", CreateCowboy).Methods("POST")
+	router.HandleFunc("/cowboys/{id}", DeleteCowboy).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
